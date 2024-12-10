9bec840e4ed4e5b2384034dbaadf3d0cfb1a661d
David24zx <david.alba24@student.cart.org>
2024-12-10 04:03:23 -0800

    config changes

--- app.js
+++ app.js
@@ -12,3 +12,30 @@ document.querySelectorAll('.options select').forEach(select => {
     console.log(`Selected: ${value}`);
   });
 });
+
+
+function selectTab(selectedTab) {
+
+  const tabs = document.querySelectorAll('.tab');
+  tabs.forEach(tab => tab.classList.remove('active'));
+
+  selectedTab.classList.add('active');
+
+  const sections = document.querySelectorAll('.main-content');
+  sections.forEach(section => section.style.display = 'none');
+
+  const selectedTabText = selectedTab.textContent.trim();
+
+  const carImage = document.querySelector('.car-image');
+
+  if (selectedTabText === 'Exterior') {
+    carImage.src = 'config-white-car.png';
+  } else if (selectedTabText === 'Interior') {
+    carImage.src = 'config-interior.png';
+  } else if (selectedTabText === 'Equipment') {
+    carImage.src = 'config-equipment.png';
+  }
+
+  document.querySelector('.main-content').style.display = 'block';
+}
+
